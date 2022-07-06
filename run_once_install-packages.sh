6e73181afe0ebf3653ddb4d68ddea2ba5b083646
Patrick Deubel <patrick.deubel@gmail.com>
2022-07-07 00:38:39 +0200

    Added script to install packages that I use

---
+++ run_once_install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo pacman -Syu base-devel git nextcloud-client tlp tlp-rdw yay
+
+sudo pacman -S chromium firefox thunderbird
+
+sudo pacman -S vim kate kile okular
+
+sudo pacman -S borg keepassxc seahorse gnome-keyring
+
+sudo pacman -S dolphin ark ranger
+
+sudo pacman -S networkmanager network-manager-applet
+
+yay -S anki spotify
