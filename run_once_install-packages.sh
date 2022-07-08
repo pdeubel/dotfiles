7b613a26e05977ea973005622b55600e05e7fff0
Patrick Deubel <patrick.deubel@gmail.com>
2022-07-08 21:58:41 +0200

    Added some more packages to install script

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -6,7 +6,7 @@ sudo pacman -Syu base-devel git nextcloud-client tlp tlp-rdw yay
 
 sudo pacman -S --noconfirm chromium firefox thunderbird
 
-sudo pacman -S --noconfirm vim kate kdeconnect kile okular
+sudo pacman -S --noconfirm vim kate kdeconnect kile okular spectacle
 
 sudo pacman -S --noconfirm borg keepassxc seahorse gnome-keyring
 
@@ -14,11 +14,11 @@ sudo pacman -S --noconfirm dolphin ark ranger
 
 sudo pacman -S --noconfirm networkmanager network-manager-applet
 
-sudo pacman -S --noconfirm nitrogen polybar rofi
+sudo pacman -S --noconfirm dunst nitrogen picom polybar rofi ttf-hack
 
 # This will also require user input, but I guess here it is fine as it
 # downloads packages from AUR
-yay -S anki rofi-greenclip spotify
+yay -S anki i3lock-fancy-rapid-git rofi-greenclip spotify
 
 # Setup instructions for TLP
 systemctl enable tlp.service
