15e5dea8dbd6434b579c51ad467f3713fb1631d8
Patrick Deubel <patrick.deubel@gmail.com>
2022-07-13 14:03:58 +0200

    Fixed zalias again, added removing of existing polybar log files when launching

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+LOG_FILE_1=/tmp/polybar1.log
+LOG_FILE_2=/tmp/polybar2.log
+
 # Terminate already running bar instances
 
 killall -q polybar
@@ -7,13 +10,15 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+# Remove log files if they exist
+
+[ -e $LOG_FILE_1 ] && rm $LOG_FILE_1
+[ -e $LOG_FILE_2 ] && rm $LOG_FILE_2
 
 # Launch bar1 and bar2
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-
-polybar top >>/tmp/polybar1.log 2>&1 &
-polybar bottom >>/tmp/polybar2.log 2>&1 &
+polybar top >> $LOG_FILE_1 2>&1 &
+polybar bottom >> $LOG_FILE_2 2>&1 &
 
 
 echo "Bars launched..."
