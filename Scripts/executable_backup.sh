656071c495288f50fae7cd71913a8c6d6eae6bf4
Patrick Deubel <patrick.deubel@gmail.com>
2022-07-13 22:45:38 +0200

    Backup script made paths as variables, added mounting by asking user for device path

--- Scripts/executable_backup.sh
+++ Scripts/executable_backup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+BACKUP_MOUNT_DIR=/run/media/Backup
+
 export BORG_PASSCOMMAND='secret-tool lookup borg-backup passphrase'
+
+sudo mkdir -p $BACKUP_MOUNT_DIR
+
+# Ask user for the device, by showing him the options
+lsblk
+echo
+echo
+read -p "Select the backup device: /dev/sd" BACKUP_DEV_USER_INPUT
+
+BACKUP_DEVICE_PATH=/dev/sd$BACKUP_DEV_USER_INPUT
+
+sudo mount $BACKUP_DEVICE_PATH $BACKUP_MOUNT_DIR
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
@@ -10,7 +24,7 @@ borg create                                                                  \
     --progress                                                               \
     --stats                                                                  \
     --patterns-from=/home/pdeubel/.config/borg/patternfile.lst               \
-    /run/media/Backup/borg-backup::$(hostname)-$(date --iso-8601='seconds')
+    $BACKUP_MOUNT_DIR/borg-backup::$(hostname)-$(date --iso-8601='seconds')
 
 # Returns exit status of the last command
 backup_exit=$?
@@ -30,7 +44,7 @@ borg prune                          \
     --keep-daily    7               \
     --keep-weekly   4               \
     --keep-monthly  6               \
-    /run/media/Backup/borg-backup
+    $BACKUP_MOUNT_DIR/borg-backup
 
 prune_exit=$?
 
@@ -38,7 +52,7 @@ prune_exit=$?
 
 info "Compacting repository"
 
-borg compact /run/media/Backup/borg-backup
+borg compact $BACKUP_MOUNT_DIR/borg-backup
 
 compact_exit=$?
 
@@ -55,5 +69,7 @@ elif [ ${global_exit} -eq 1 ]; then
 else
     info "Backup, Prune, and/or Compact finished with errors"
 fi
+
+sudo umount $BACKUP_DEVICE_PATH
 
 exit ${global_exit}
