7d5fd69584b05302907eb34034236e6c033945fa
Patrick Deubel <33164686+pdeubel@users.noreply.github.com>
2026-03-24 22:42:17 +0100

    feat: Increase history sizes, update display setup, fix font, and some more

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/pdeubel/.zsh/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/pdeubel/.zsh/fzf/bin"
+  PATH="${PATH:+${PATH}:}/home/pdeubel/.zsh/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/pdeubel/.zsh/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/home/pdeubel/.zsh/fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
